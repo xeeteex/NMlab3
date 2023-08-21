914b491d0541b5b7fd3d8244b27f02e80a4d8f73
xeeteex <136630215+xeeteex@users.noreply.github.com>
2023-08-21 22:59:51 +0545

    Create compositetrap.c

---
+++ compositetrap.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<math.h>
+/* Define function here */
+#define f(x) pow(x,4)+x+2
+
+int main()
+{
+ float lower, upper, integration=0.0, stepSize, k;
+ int i, subInterval;
+ printf("Enter lower limit of integration: ");
+ scanf("%f", &lower);
+ printf("Enter upper limit of integration: ");
+ scanf("%f", &upper);
+ printf("Enter number of sub intervals: ");
+ scanf("%d", &subInterval);
+
+ 
+ /* Finding step size */
+ stepSize = (upper - lower)/subInterval;
+
+ /* Finding Integration Value */
+ integration = f(lower) + f(upper);
+ for(i=1; i<= subInterval-1; i++)
+ {
+  k = lower + i*stepSize;
+  integration = integration + 2 * f(k);
+ }
+ integration = integration * stepSize/2;
+ printf("\nRequired value of integration is: %.3f", integration);
+ return 0;
+}
