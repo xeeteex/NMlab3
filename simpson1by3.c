91f6eba15eebb360d31ad9e8ef74033a6611c28b
xeeteex <136630215+xeeteex@users.noreply.github.com>
2023-08-21 23:06:52 +0545

    Create simpson1by3.c

---
+++ simpson1by3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+/* Define function here */
+#define f(x) pow(x,4)+x+2
+
+int main()
+{
+ float h,i,j,I,lower, upper, integration=0.0 ;
+
+ /* Input */
+ printf("Enter lower limit of integration: ");
+ scanf("%f", &lower);
+ printf("Enter upper limit of integration: ");
+ scanf("%f", &upper);
+    
+ /* Finding Integration Value */
+    integration= f(lower) + f(upper);
+    h= (upper-lower)/2;
+//    printf("f(x0) and f(x2) =%.3f  ,  %.3f", f(lower), f(upper));
+ {
+     i=4*f(lower+h);
+     j=i+integration;
+ }
+ I= (h*j)/3;
+ printf("\nRequired value of integration is: %.3f", I);
+
+ return 0;
+}
